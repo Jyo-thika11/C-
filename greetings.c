d6e7b1c8fba6d31ccd8e7bd5b3c60ba9f18fc7c0
Jyo-thika11 <saijyothikasigireddy@gmail.com>
2026-02-13 12:43:39 +0530

    Add greetings.c to print meal messages

---
+++ greetings.c
@@ -0,0 +1,14 @@
+#include <stdio.h> // Include standard input-output library
+
+int main() {
+    {
+        printf("Breakfast, "); // Block for printing breakfast message
+    }
+    {
+        printf("Lunch, "); // Block for printing lunch message
+    }
+    {
+        printf("Dinner!"); // Block for printing dinner message
+    }
+    return 0;
+}
