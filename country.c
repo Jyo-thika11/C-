2967f6075e8375a1ab4f5f4290b23f78128a7e4a
Jyo-thika11 <saijyothikasigireddy@gmail.com>
2026-02-15 10:30:44 +0530

    country.c

---
+++ country.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Country:\tCapital:\n");
+    printf("India\t\tNew Delhi\n");     
+    printf("France\t\tParis\n");       
+    printf("Japan\t\tTokyo\n");       
+
+    return 0; 
+}
