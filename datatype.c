0eb23afcddde7df93e5ef9cd37beec8c4135d3cd
Jyo-thika11 <saijyothikasigireddy@gmail.com>
2026-02-15 19:46:45 +0530

    datatype.c

---
+++ datatype.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    short birthYear = 2005;
+    int studentID = 102345;
+    printf("Year of Birth: %d\n", birthYear);
+    printf("Student ID: %d", studentID);
+
+    return 0;
+}
