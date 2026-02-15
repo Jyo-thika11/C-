ce1c8a7384c4ef1461293ceca545254f31e65ea0
Jyo-thika11 <saijyothikasigireddy@gmail.com>
2026-02-15 10:43:40 +0530

    quote.c

---
+++ quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    {
+    printf("Displaying a famous quote:\n");
+    }
+    {
+    printf("\"Success is not final,\n");
+    printf("failure is not  fatal:\n");
+    printf("It is the courage to continue that counts.\"");
+    return 0;
+    }
+}
